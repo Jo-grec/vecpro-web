7e0ba358b98566a57c4d6b7577f7714ff1f2b1b2
Jo-grec <joelmargrecia15@gmail.com>
2026-09-02 23:46:56 +0800

    style(platform): fix logos spacing

--- src/components/platform.tsx
+++ src/components/platform.tsx
@@ -83,7 +83,7 @@ export default function Platform() {
             The VECPRO Ecosystem
           </p>
 
-          <div className="mt-4 mb-10 flex w-full items-center justify-center gap-2 sm:gap-4 lg:gap-8">
+          <div className="mt-4 mb-10 flex w-full items-center justify-center gap-1 sm:gap-3 lg:gap-6">
 
             <img
               src="/logo/aedex.png"
